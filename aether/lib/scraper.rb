d8a4d6dd22d11bd772b1bd29f4f0c6e6f4467047
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-05 11:50:24 +0000

    Done.

--- aether/lib/scraper.rb
+++ aether/lib/scraper.rb
@@ -4,31 +4,31 @@ require 'pry'
 
 require_relative './apartment.rb'
 
+
 class Scraper
 
+  attr_reader :link, :name, :background
+
   def self.get_page
-  #  doc = Nokogiri::HTML(open("https://www.glenwoodnyc.com/"))
-  doc = Nokogiri::HTML(open("https://www.calibernyc.com/nyc-rentals"))
-
-  #  doc = doc.css("div.new-featured-l div.col-sm-12 div.guide_box_fa.pagination_list")
-    doc = doc.css("div.portfolio.properties")
-
-  #  doc.css("div.portfolio.properties").css("div.image-caption").css.("a")
-
+  #  doc = Nokogiri::HTML(open("https://www.calibernyc.com/nyc-rentals"))
+  #  doc = doc.css("div.portfolio.properties").css("div.image-caption")
+    doc = Nokogiri::HTML(open("https://www.calibernyc.com/queens/house-for-rent/39-14-54-st-1/1273804"))
+    binding.pry
+  #  doc = doc.css("p.property-detail-features.content-wrapper")
   end
 
   def self.scraped_products
       self.get_page.each do |element|
-binding.pry
+        binding.pry
     #  apartment = Apartment.new
-      name = element.children[1].children.text
-      link = element.css(".img src")
-      background = apartment_background
+      @name = element.css("a")[0]
+      @link = element.css("a")[0]
+      @background = apartment_background
     end
   end
 
   def apartment_background(link)
-    doc = Nokogiri::HTML(open("apartment.link"))
+    doc = Nokogiri::HTML(open("link"))
     doc = doc.css("p")
     puts "#{doc}"
   end
