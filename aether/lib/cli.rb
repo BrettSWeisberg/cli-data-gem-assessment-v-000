5a3465ffa8b3f48bdb3be68e97004d7d6c28f842
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-07 01:09:57 +0000

    Done.

--- aether/lib/cli.rb
+++ aether/lib/cli.rb
@@ -17,13 +17,13 @@ class Cli
     puts "Please select an apartment to see more info by typing in the corresponding number."
     input = gets.strip
     apt_num = input.to_i - 1
-    if  apt_num.between?(0, max)
+    if apt_num.between?(0, max)
       the_apt_name = Apartment.all[apt_num].name
       the_apt_link = Apartment.all[apt_num].link
       the_apt_background = Apartment.all[apt_num].background
 
       puts "You have selected: #{the_apt_name}"
-      puts  "Link: #{the_apt_link}"
+      puts "Link: #{the_apt_link}"
       puts "Background: #{the_apt_background}"
     else
       puts "Please re-launch CLI and enter corresponding number. Goodbye!"
